2f2ee2c723a388ed0e03857b2a4cd8b1fda6147c
Jake Yoo <jyoo2001@gmail.com>
2023-02-16 13:59:42 -0600

    new home page, other features listed in desc

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ app = Flask(__name__)
 @app.route("/", methods=['GET', 'POST'])
 def login():
     if request.method == 'GET':
-        return render_template('login.html')
+        return render_template('home.html')
     elif request.method == 'POST':
         return "hi"
