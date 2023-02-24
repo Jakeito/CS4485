fed3bb6e76efd5f07ad91ae8a0b472910f3a203f
Jake Yoo <jyoo2001@gmail.com>
2023-02-24 00:33:20 -0600

    Made some UI changes, implemented some pages

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, redirect
 import json
 app = Flask(__name__)
 
-@app.route("/", methods=['GET', 'POST'])
-def login():
-    if request.method == 'GET':
-        return render_template('home.html')
-    elif request.method == 'POST':
-        return "hi"
+@app.route('/', methods=['GET'])
+def redir():
+    return redirect('/home')
+
+@app.route('/home', methods=['GET'])
+def home():
+    return render_template('home.html')
+
+@app.route('/about', methods=['GET'])
+def about():
+    return render_template('about.html')
+
+@app.route('/register', methods=['GET'])
+def register():
+    return render_template('register.html')
+
+@app.route('/signin', methods=['GET'])
+def signin():
+    return render_template('signin.html')
