576453da39a4855dd842f90900b04a05b9c76556
Jake Yoo <jyoo2001@gmail.com>
2023-02-24 00:34:50 -0600

    forgot to add files again

---
+++ static/about.js
@@ -0,0 +1,12 @@
+document.getElementById("home").onclick = function () {
+    location.href = "/home";
+};
+document.getElementById("about").onclick = function () {
+    location.href = "/about";
+};
+document.getElementById("suppSubj").onclick = function () {
+    location.href = "/suppSubj";
+};
+document.getElementById("signin").onclick = function () {
+    location.href = "/signin";
+};
