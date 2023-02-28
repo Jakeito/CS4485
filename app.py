046b6e7d154837ea3f2c5437f893c6aabef503aa
Kush <kushpatel2001@gmail.com>
2023-02-28 17:37:42 -0600

    password encryption checks if the password is strong

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, jsonify, redirect
 import json
+import hashlib
+
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
@@ -21,3 +23,35 @@ def register():
 @app.route('/signin', methods=['GET'])
 def signin():
     return render_template('signin.html')
+
+
+##checks if the password contains 12 character, upper and lower case character, and a number
+##returns a boolean and sends a message to front end display
+def strongPWD (pwd):
+    check = True
+    weakPass = ""
+    
+    if len(pwd) < 12:
+        check = False
+        weakPass += "Password needs to be at least 12 character. "
+
+    if pwd.islower() or pwd.isupper():
+        check = False
+        weakPass += "Password needs at least 1 upper and 1 lower case character. "
+    
+    if any(i.isdigit() for i in pwd) == False:
+        check = False
+        weakPass += "Password needs at least one number. "
+
+    if check:
+        return "Strong"
+    else:
+        return weakPass
+
+    return check
+
+##returns an encrypted password
+def encrypt (pwd):
+    newPwd = hashlib.sha256(pwd.encode())
+    newPwd = newPwd.hexdigest()
+    return newPwd
