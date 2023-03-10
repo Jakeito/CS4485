ed591ce905f2eacfb9512294437e673ad86d532f
Jake Yoo <jyoo2001@gmail.com>
2023-03-09 21:58:29 -0600

    signin setup and most of register complete

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, request, jsonify, redirect
 import json
+import hashlib
+from flask_login import *
+
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
@@ -14,10 +17,20 @@ def home():
 def about():
     return render_template('about.html')
 
-@app.route('/register', methods=['GET'])
-def register():
-    return render_template('register.html')
+@app.route('/register-student', methods=['GET'])
+def register_student():
+    return render_template('register-student.html')
+
+@app.route('/register-tutor', methods=['GET'])
+def register_tutor():
+    return render_template('register-tutor.html')
 
 @app.route('/signin', methods=['GET'])
 def signin():
     return render_template('signin.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect('/home')
--- static/signin.js
+++ static/signin.js
@@ -1,15 +1,37 @@
-document.getElementById("home").onclick = function () {
-    location.href = "/home";
+document.getElementById('home').onclick = function () {
+    location.href = '/home';
 };
-document.getElementById("about").onclick = function () {
-    location.href = "/about";
+document.getElementById('about').onclick = function () {
+    location.href = '/about';
 };
-document.getElementById("suppSubj").onclick = function () {
-    location.href = "/suppSubj";
+document.getElementById('suppSubj').onclick = function () {
+    location.href = '/suppSubj';
 };
-document.getElementById("signin").onclick = function () {
-    location.href = "/signin";
+document.getElementById('signin').onclick = function () {
+    location.href = '/signin';
 };
-document.getElementById("signup").onclick = function () {
-    location.href = "/register";
+document.getElementById('signup').onclick = function () {
+    location.href = '/register-student';
 };
+
+$(document).ready(function () {
+    $('#signin-button').click(signin)
+})
+
+function signin() {
+    const netID = $('#input-net-id').val()
+    const password = $('#input-password').val()
+    console.log(netID + '\n' + password)
+    const response = fetch('/api/signin', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            'net-id': netID,
+            'password': password
+        })
+    })
+
+    //If login is successful create cookie and redirect to home page with new buttons
+}
