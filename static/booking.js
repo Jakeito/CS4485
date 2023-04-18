f52093dbbcb5304835113e9ec30429a41f88fa48
squimoo <61803711+squimoo@users.noreply.github.com>
2023-04-18 03:49:59 -0500

    adding booking.js

---
+++ static/booking.js
@@ -0,0 +1,39 @@
+document.getElementById("home").onclick = function () {
+    location.href = "/home";
+};
+document.getElementById("about").onclick = function () {
+    location.href = "/about";
+};
+document.getElementById("suppSubj").onclick = function () {
+    location.href = "/suppSubj";
+};
+if (document.getElementById("requestAppointment") !== null) {
+    document.getElementById("requestAppointment").onclick = function () {
+        location.href = "/appointment";
+    };
+}
+if (document.getElementById("signin") !== null) {
+    document.getElementById("signin").onclick = function () {
+        location.href = "/signin";
+    };
+}
+if (document.getElementById("profile") !== null ) {
+    document.getElementById("profile").onclick = function () {
+        fetch('/api/net-id').then(response=>response.text())
+        .then(data=>{
+            location.href = "/profile/" + data;
+        })
+    };
+}
+if (document.getElementById("logout") !== null) {
+    document.getElementById("logout").onclick = function () {
+        location.href = "/logout";
+    };
+}
+
+if (document.getElementById("submit") !== null) {
+    document.getElementById("submit").onclick = function () {
+        const dateTime = flatpickr("#meeting-date-time", {});
+        console.log(dateTime.selectedDates);
+    };
+}
